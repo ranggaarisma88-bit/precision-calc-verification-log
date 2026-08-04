37fe570577c49a340dfb99d356f07d1713cf5ad8
ranggaarisma88-bit <ranggaarisma88@gmail.com>
2026-08-04 09:47:22 +0700

    Precision Calc And Verification Log

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'precision-calc-v2.0';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/sw.js',
+    '/icon-192.png',
+    '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(ASSETS).catch(err => {
+                console.error('[SW] Cache failed:', err);
+            });
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        Promise.all([
+            caches.keys().then(keys => {
+                return Promise.all(
+                    keys.filter(key => key !== CACHE_NAME)
+                        .map(key => caches.delete(key))
+                );
+            }),
+            clients.claim()
+        ])
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+            return cachedResponse || fetch(event.request);
+        })
+    );
+});
